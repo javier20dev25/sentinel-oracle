4fd4e2ef1ffd9182675bbc193a2cfc0f1b867976
Javier Astaroth <sleytherjavier2025@gmail.com>
2026-07-14 19:40:08 -0600

    chore: remove stray scratch scripts, update .gitignore for start scripts

--- start-server.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "C:\Users\sleyt\sentinel-oracle"
-"C:\Program Files\nodejs\node.exe" dist\index.js > "C:\Users\sleyt\.sentinel-oracle\debug-server.log" 2>&1
--- start.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d C:\Users\sleyt\sentinel-oracle
-node dist\index.js
